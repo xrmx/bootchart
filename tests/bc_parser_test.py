0ca8e1e6aa75d2ec9057d505ded25379c780f9f3
anders.norgaard <anders.norgaard@934cfe9c-cbc0-11dd-a880-a1081c7ac358>
2008-12-28 13:01:04 +0000

    Fix stupid whitespace mixup
    git-svn-id: http://pybootchartgui.googlecode.com/svn/trunk@14 934cfe9c-cbc0-11dd-a880-a1081c7ac358

--- tests/bc_parser_test.py
+++ tests/bc_parser_test.py
@@ -9,57 +9,61 @@ import bc_parser
 
 class TestBCParser(unittest.TestCase):
     
-    def setUp(self):
-        self.name = "My first unittest"
+	def setUp(self):
+		self.name = "My first unittest"
 
-    def testParseProcPsLog(self):    
-	samples = bc_parser.parseProcPsLog('examples/1/proc_ps.log', {})
+	def testParseProcPsLog(self):    
+		samples = bc_parser.parseProcPsLog('examples/1/proc_ps.log', {})
 
-	print samples[2]
-	print samples[3]	
-	print len(samples[0])
+		print samples[2]
+		print samples[3]	
+		print len(samples[0])
 	
-	processes = samples[0]
-	sorted_processes = sorted(processes, key=lambda p: p.pid )
-	
-	for index, line in enumerate(open('examples/1/extract2.proc_ps.log')):
-		tokens = line.split();
-		process = sorted_processes[index]
-		print tokens[0:4]
-		print process.pid, process.cmd, process.ppid, len(process.samples)
-		print '-------------------'
-		#if len(process.samples) > 0: break
-		self.assertEqual(tokens[0], str(process.pid))
-		self.assertEqual(tokens[1], str(process.cmd))
-		self.assertEqual(tokens[2], str(process.ppid))
-		self.assertEqual(tokens[3], str(len(process.samples)))
+		processes = samples[0]
+		sorted_processes = sorted(processes, key=lambda p: p.pid )
+		
+		for index, line in enumerate(open('examples/1/extract2.proc_ps.log')):
+			tokens = line.split();
+			process = sorted_processes[index]
+			print tokens[0:4]
+			print process.pid, process.cmd, process.ppid, len(process.samples)
+			print '-------------------'
+			#if process.pid != process.ppid: break
+			self.assertEqual(tokens[0], str(process.pid))
+			self.assertEqual(tokens[1], str(process.cmd))
+			self.assertEqual(tokens[2], str(process.ppid))
+			self.assertEqual(tokens[3], str(len(process.samples)))
         
 
-    def testparseProcStatLog(self):
-    	if self:
-    		return
-	samples = bc_parser.parseProcDiskStatLog(2, 'examples/1/proc_diskstats.log')
-	self.assertEqual(282, len(samples))
+	def testparseProcDiskStatLog(self):
+		if self:
+			return
+		samples = bc_parser.parseProcDiskStatLog(2, 'examples/1/proc_diskstats.log')
+		self.assertEqual(282, len(samples))
 	
-	sample1 = samples[0]
-	self.assertEqual(0.0, sample1.read)
-	self.assertEqual(0.0, sample1.write)
-	self.assertEqual(176, sample1.time)
+		sample1 = samples[0]
+		self.assertEqual(0.0, sample1.read)
+		self.assertEqual(0.0, sample1.write)
+		self.assertEqual(176, sample1.time)
 	
-	for index, line in enumerate(open('examples/1/extract.proc_diskstats.log')):
-		tokens = line.split('\t')
-		sample = samples[index]
-		print line.rstrip()
-		print sample
-		print '-------------------'
-		self.assertEqual(line.rstrip(), str(sample))
+		for index, line in enumerate(open('examples/1/extract.proc_diskstats.log')):
+			tokens = line.split('\t')
+			sample = samples[index]
+			print line.rstrip()
+			print sample
+			print '-------------------'
+			self.assertEqual(line.rstrip(), str(sample))
 		
-	sample200 = samples[200]
-	self.assertEqual(1620.0, sample200.read)
-	self.assertEqual(0.0, sample200.write)
-	self.assertEqual(2216, sample200.time)
+		sample200 = samples[200]
+		self.assertEqual(1620.0, sample200.read)
+		self.assertEqual(0.0, sample200.write)
+		self.assertEqual(2216, sample200.time)
 	
-	#self.assert_(element in self.seq)
+		#self.assert_(element in self.seq)
+
+	
+	def testparseProcStatLog(self):
+		pass
 
 if __name__ == '__main__':
     unittest.main()

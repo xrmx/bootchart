c96601b1eb92c02e185eb5cb5f767f02bf0eb1a8
anders.norgaard <anders.norgaard@934cfe9c-cbc0-11dd-a880-a1081c7ac358>
2009-01-02 20:01:25 +0000

    Clean up tests
    git-svn-id: http://pybootchartgui.googlecode.com/svn/trunk@19 934cfe9c-cbc0-11dd-a880-a1081c7ac358

--- tests/bc_parser_test.py
+++ tests/bc_parser_test.py
@@ -6,6 +6,10 @@ print sys.path.insert(0, os.getcwd())
 
 import bc_parser
 
+debug = False
+
+def floatEq(f1, f2):
+	return math.fabs(f1-f2) < 0.00001
 
 class TestBCParser(unittest.TestCase):
     
@@ -13,24 +17,19 @@ class TestBCParser(unittest.TestCase):
 		self.name = "My first unittest"
 
 	def testParseProcPsLog(self):
-		if self:
-			return
 		samples = bc_parser.parseProcPsLog('examples/1/proc_ps.log', {})
 
-		print samples[2]
-		print samples[3]	
-		print len(samples[0])
-	
 		processes = samples[0]
 		sorted_processes = sorted(processes, key=lambda p: p.pid )
 		
 		for index, line in enumerate(open('examples/1/extract2.proc_ps.log')):
 			tokens = line.split();
 			process = sorted_processes[index]
-			print tokens[0:4]
-			print process.pid, process.cmd, process.ppid, len(process.samples)
-			print '-------------------'
-			#if process.pid != process.ppid: break
+			if debug:	
+				print tokens[0:4]
+				print process.pid, process.cmd, process.ppid, len(process.samples)
+				print '-------------------'
+			
 			self.assertEqual(tokens[0], str(process.pid))
 			self.assertEqual(tokens[1], str(process.cmd))
 			self.assertEqual(tokens[2], str(process.ppid))
@@ -38,52 +37,38 @@ class TestBCParser(unittest.TestCase):
         
 
 	def testparseProcDiskStatLog(self):
-		if self:
-			return
 		samples = bc_parser.parseProcDiskStatLog(2, 'examples/1/proc_diskstats.log')
 		self.assertEqual(141, len(samples))
-	
-		sample1 = samples[0]
-		self.assertEqual(0.0, sample1.read)
-		self.assertEqual(0.0, sample1.write)
-		self.assertEqual(176, sample1.time)
 	
 		for index, line in enumerate(open('examples/1/extract.proc_diskstats.log')):
 			tokens = line.split('\t')
 			sample = samples[index]
-			print line.rstrip(), 
-			print sample
-			print '-------------------'
-			self.assertEqual(line.rstrip(), str(sample))
-		
-		sample200 = samples[200]
-		self.assertEqual(1620.0, sample200.read)
-		self.assertEqual(0.0, sample200.write)
-		self.assertEqual(2216, sample200.time)
-	
-		#self.assert_(element in self.seq)
-
+			if debug:		
+				print line.rstrip(), 
+				print sample
+				print '-------------------'
+			
+			self.assertEqual(tokens[0], str(sample.time))
+			self.assert_(floatEq(float(tokens[1]), sample.read))
+			self.assert_(floatEq(float(tokens[2]), sample.write))
+			self.assert_(floatEq(float(tokens[3]), sample.util))
 	
 	def testparseProcStatLog(self):
 		samples = bc_parser.parseProcStatLog('examples/1/proc_stat.log')
 		self.assertEqual(141, len(samples))
-
-		def floatEq(f1, f2):
-			return math.fabs(f1-f2) < 0.00001
 			
 		for index, line in enumerate(open('examples/1/extract.proc_stat.log')):
 			tokens = line.split('\t')
 			sample = samples[index]
-			print line.rstrip()
-			print sample
-			print '-------------------'
+			if debug:
+				print line.rstrip()
+				print sample
+				print '-------------------'
 			self.assert_(floatEq(float(tokens[0]), sample.time))
 			self.assert_(floatEq(float(tokens[1]), sample.user))
 			self.assert_(floatEq(float(tokens[2]), sample.sys))
 			self.assert_(floatEq(float(tokens[3]), sample.io))
-		
-		pass
-
+	
 if __name__ == '__main__':
     unittest.main()
 

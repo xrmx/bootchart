4a7799bb516328a548993cc29627d45a01faa22b
henningniss <henningniss@934cfe9c-cbc0-11dd-a880-a1081c7ac358>
2009-01-18 20:09:50 +0000

    Added preliminary options support. For now the options do not do anything.
    git-svn-id: http://pybootchartgui.googlecode.com/svn/trunk@77 934cfe9c-cbc0-11dd-a880-a1081c7ac358

--- src/main.py
+++ src/main.py
@@ -1,11 +1,45 @@
 import sys
 import os
+import optparse
 
 import parsing
 import gui
 
 sys.path.insert(0, os.getcwd())
 
+def _mk_options_parser():
+	"""Make an options parser."""
+	usage = "%prog [options] PATH, ..., PATH"
+	version = "%prog v0.0.0"
+	parser = optparse.OptionParser(usage, version=version)
+	parser.add_option("-f", "--format", dest="format",
+			  help="image format (...); default format ...")
+	parser.add_option("-o", "--output-dir", dest="outputdir", metavar="DIR", default=".",
+			  help="output directory where images are stored (default: .)")
+	parser.add_option("-n", "--no-prune", action="store_true", dest="noprune", default=False,
+			  help="do not prune the process tree")
+	parser.add_option("-q", "--quiet", action="store_true", dest="quiet", default=False,
+			  help="suppress informational messages")
+	parser.add_option("--very-quiet", action="store_true", dest="veryquiet", default=False,
+			  help="suppress all messages except errors")
+	parser.add_option("--verbose", action="store_true", dest="verbose", default=False,
+			  help="print all messages")
+	return parser
+
+def main(argv=None):
+	if argv is None:
+		argv = sys.argv[1:]
+	
+	parser = _mk_options_parser()
+	opts, args = parser.parse_args(argv)
+	
+	if len(args) == 0:
+		parser.error("insufficient arguments, expected at least one path.")
+		return 2
+
+	res = parsing.parse_log_dir(args[0], True)
+	gui.show(res)
+	return 0
+
 if __name__ == '__main__':
-	res = parsing.parse_log_dir(sys.argv[1], True)
-        gui.show(res)
+	sys.exit(main())

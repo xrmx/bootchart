4bd596b0e41df99df6332030c502b0af9e20ee0e
henningniss <henningniss@934cfe9c-cbc0-11dd-a880-a1081c7ac358>
2009-01-20 13:33:58 +0000

    Slightly more intuitive naming of produced chart outputs.
    git-svn-id: http://pybootchartgui.googlecode.com/svn/trunk@81 934cfe9c-cbc0-11dd-a880-a1081c7ac358

--- pybootchartgui/main.py
+++ pybootchartgui/main.py
@@ -15,8 +15,8 @@ def _mk_options_parser():
 			  help="start in active mode")
 	parser.add_option("-f", "--format", dest="format", default = None,
 			  help="image format (...); default format ...")
-	parser.add_option("-o", "--output-dir", dest="outputdir", metavar="DIR", default=".",
-			  help="output directory where images are stored (default: .)")
+	parser.add_option("-o", "--output", dest="output", metavar="PATH", default=None,
+			  help="output path (file or directory) where charts are stored")
 	parser.add_option("-n", "--no-prune", action="store_false", dest="prune", default=True,
 			  help="do not prune the process tree")
 	parser.add_option("-q", "--quiet", action="store_true", dest="quiet", default=False,
@@ -26,6 +26,18 @@ def _mk_options_parser():
 	parser.add_option("--verbose", action="store_true", dest="verbose", default=False,
 			  help="print all messages")
 	return parser
+
+def _get_filename(paths, options):
+	"""Construct a usable filename for outputs based on the paths and options given on the commandline."""
+	dir = ""
+	file = "bootchart"
+	if options.output != None and not(os.path.isdir(options.output)):
+		return options.output
+	if options.output != None:
+		dir = options.output
+	if len(paths) == 1 and os.path.isdir(paths[0]):
+		file = os.path.split(paths[0])[-1]
+	return os.path.join(dir, file + "." + options.format)
 
 def main(argv=None):
 	if argv is None:
@@ -42,7 +54,9 @@ def main(argv=None):
 	if options.interactive or options.format == None:
 		gui.show(res)
 	else:
-		batch.render(res, options.format, "bootchart." + options.format)
+		filename = _get_filename(args, options)
+		batch.render(res, options.format, filename)
+		print "bootchart written to", filename
 	return 0
 
 

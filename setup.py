b415d080454daf9161ea097e57e579c2b61a2759
Michael Meeks <michael.meeks@novell.com>
2010-04-26 15:31:13 +0100

    Remove $Id$ tag, it causes an error because it isn't substituted by the new upstream VCS (git). Thanks to: Kel Modderman <kel@otaku42.de>

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from distutils.core import setup
 
-idStr = '$Id$'
-
 setup(name = 'pybootchartgui',
-      version = 'r' + idStr.split()[2],
       description = 'Python bootchart graph utility',
 
       packages = ['pybootchartgui'],

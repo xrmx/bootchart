89cee25e3cd7c472529c3e544c768129e33f92b6
Sangjung Woo <sangjung.woo@samsung.com>
2013-05-31 08:01:55 -0700

    Add detailed log() and assert() functions
    In order to easy debug, this patch makes detailed log() and assert()
    functions when 'CONFIG_DEBUG' is defined. For example, CPPFLAGS has
    '-DCONFIG_DEBUG', log message shows you its file name and line number.
    This patch also overrides glibc assert() for adding detailed message.
    
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

---
+++ collector/macro.h
@@ -0,0 +1,28 @@
+#ifndef BOOTCHART_MACRO_H
+#define BOOTCHART_MACRO_H
+
+#include <assert.h>
+
+/* In order to easy debug, override glibc assert() and define log()
+ * function
+ */
+#undef assert
+#define PREFIX				"[bootchart] "
+#ifdef CONFIG_DEBUG
+#include <stdlib.h>
+#define log_debug(fmt,...)		fprintf(stderr, PREFIX "%s:%d " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+#define assert_debug(expr)				\
+	do {						\
+		if (unlikely(!(expr))){			\
+			log_debug("Assertion failed");	\
+			abort();			\
+		}					\
+	} while(0)
+#define log			log_debug
+#define assert(expr)		assert_debug(expr)
+#else
+#define log(fmt,...)		fprintf(stderr, PREFIX fmt, ##__VA_ARGS__)
+#define assert(expr)		do {} while(0)
+#endif /* CONFIG_DEBUG */
+
+#endif /* BOOTCHART_MACRO_H */
